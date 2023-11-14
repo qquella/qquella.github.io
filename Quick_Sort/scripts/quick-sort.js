b59a232e6e325c9eb132defeb606882c4f72704d
Quella <132938295+qquella@users.noreply.github.com>
2023-11-14 17:01:08 +0200

    qs done yay

--- Quick_Sort/scripts/quick-sort.js
+++ Quick_Sort/scripts/quick-sort.js
@@ -44,9 +44,22 @@ function toggleText() {
   
       hiddenTextElements[textIndex].style.display = hiddenTextElements[textIndex].style.display === 'none' ? 'block' : 'none';
 
-      textIndex ++;
+      textIndex ++;  
   }
 
+  // Toggle the font size when clicking on .shrink
+  const toShrink1 = Array.from(document.querySelectorAll('.shrink'));
+  const toShrink2 = Array.from(document.querySelectorAll('.shrink span'));
+  toShrink1.forEach(ele => {
+      ele.addEventListener('click', () => {
+          ele.style.fontSize = '1em';
+      });
+  });
+  toShrink2.forEach(ele => {
+    ele.addEventListener('click', () => {
+        ele.style.fontSize = '2em';
+    });
+  });
 
 // Initial setup
 document.addEventListener('DOMContentLoaded', function() {
@@ -58,3 +71,36 @@ document.addEventListener('DOMContentLoaded', function() {
 
     showSlide(currentSlide);
 });
+
+//For Math display
+// Function to calculate k based on the initial input
+function calculateK() {
+  // Get the values of n and t from the input fields
+  const n = parseFloat(document.getElementById('input-n').value);
+  const t = parseFloat(document.getElementById('input-t').value);
+
+  // Calculate k using the formula k = t / n^2
+  const k = t / (n * n);
+
+  // Display the calculated k value in MathJax format
+  document.getElementById('result-k').textContent = `\\(${k}\\)`;
+  // Render MathJax
+  MathJax.typeset();
+}
+
+// Function to calculate t based on subsequent input
+function calculateT() {
+  // Get the values of n from the input field
+  const n2 = parseFloat(document.getElementById('input-n2').value);
+
+  // Get the previously calculated value of k
+  const k = parseFloat(document.getElementById('result-k').textContent);
+
+  // Calculate t using the formula t = k * n^2
+  const t2 = k * (n2 * n2);
+
+ // Display the calculated t value in MathJax format
+ document.getElementById('result-t2').textContent = `\\(${t2}\\)`;
+ // Render MathJax
+ MathJax.typeset();
+}
